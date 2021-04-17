48c2bed2d6d76af8b8778267562e3d08de340795
Yoga Sadhewo Shalahudin <82265268+YogaSadhewoS@users.noreply.github.com>
2021-04-17 10:28:05 +0700

    Bismillah fix

--- Tugas_Modul_4_C++.cpp
+++ Tugas_Modul_4_C++.cpp
@@ -27,7 +27,7 @@ int bayar (int saldo, int harga){
 
 int main(){
 	string password;
-	int i=0, saldo, jawaban;
+	int i=0, saldo, jawaban, jumlah;
 	while (i<3){
 		cout<< "Masukkan password : ";
 		cin>> password;
@@ -47,10 +47,54 @@ int main(){
 		cin>> jawaban;
 		switch(jawaban){
 			case 1:
-				cout<< "Pesanan berhasil, saldo tinggal Rp. "<<bayar(100000, 25000)<<endl;
+				cout << "Mau beli berapa kak ayamnya?" << endl;
+			cout << "Jumlah : ";
+				cin >> jumlah;
+				switch (jumlah) {
+				case 1:
+					cout << "Mau beli 1 aja? Oke, tunggu sebentar yak. Pesanan berhasil, saldo anda tinggal Rp. " << bayar(100000, 25000) << endl;
+					break;
+				case 2:
+					cout << "Dua potong ayam aja ya. Pesanan berhasil, saldo anda tinggal Rp. " << bayar(100000, 50000) << endl;
+					break;
+				case 3:
+					cout << "Ayamnya 3 ya kak, tunggu sebentar ya. Pesanan berhasil, saldo anda tinggal Rp. " << bayar(100000, 75000) << endl;
+					break;
+				case 4:
+					cout << "Widih mantap beli 4, tunggu sebentar ya kak. Pesanan berhasil, saldo anda tinggal Rp. " << bayar(100000, 100000) << endl;
+					break;
+				default:
+					cout << "Mohon maaf kak uangnya kurang" << endl;
+					break;
+				}
 				break;
 			case 2:
-				cout<< "Pesanan berhasil, saldo tinggal Rp. "<<bayar(100000, 15000)<<endl;
+				cout << "Ayam gepreknya mau beli berapa kak?" << endl;
+			cout << "Jumlah : ";
+				cin >> jumlah;
+				switch (jumlah) {
+				case 1:
+					cout << "Mau beli 1 aja? Oke, tunggu sebentar yak. Pesanan berhasil, saldo anda tinggal Rp. " << bayar(100000, 15000) << endl;
+					break;
+				case 2:
+					cout << "Ayam gepreknya 2 ya. Pesanan berhasil, saldo anda tinggal Rp. " << bayar(100000, 30000) << endl;
+					break;
+				case 3:
+					cout << "Ayam geprek 3. Tunggu sebentar ya kak. Pesanan berhasil, saldo anda tinggal Rp. " << bayar(100000, 45000) << endl;
+					break;
+				case 4:
+					cout << "Widih mantap beli 4, tunggu bentar ya kak. Pesanan berhasil, saldo anda tinggal Rp. " << bayar(100000, 60000) << endl;
+					break;
+				case 5:
+					cout << "Beli 5? Oke siapp kak bentar yak. Pesanan berhasil, saldo anda tinggal Rp. " << bayar(100000, 75000) << endl;
+					break;
+				case 6:
+					cout << "Beli 6 kak? Mantapp diborong semua. Pesanan berhasil, saldo anda tinggal Rp. " << bayar(100000, 90000) << endl;
+					break;
+				default:
+					cout << "Mohon maaf kak uangnya kurang" << endl;
+					break;
+				}
 				break;
 			case 3:
 				cout<<bayar(100000, 1000000)<<endl;
